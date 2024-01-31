7a3f7592b879c912dc570a5414a5abb5b9ba8233
markti <markti@outlook.com>
2024-01-31 09:09:40 -0500

    keyvault

--- src/terraform/keyvault.tf
+++ src/terraform/keyvault.tf
@@ -26,7 +26,7 @@ resource "azurerm_private_dns_zone" "keyvault" {
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
   name                  = azurerm_key_vault.main.name
   resource_group_name   = azurerm_resource_group.main.name
-  private_dns_zone_name = azurerm_private_dns_zone.acr.name
+  private_dns_zone_name = azurerm_private_dns_zone.keyvault.name
   virtual_network_id    = azurerm_virtual_network.main.id
 }
 
