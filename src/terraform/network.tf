c1a501fe1ede72dfa210c57101e0635a3293b001
markti <markti@outlook.com>
2024-01-30 22:11:17 -0500

    destroy

--- src/terraform/network.tf
+++ src/terraform/network.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "gateway" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [cidrsubnet(var.address_space, 5, 0)]
+  address_prefixes     = [cidrsubnet(var.address_space, 2, 0)]
 
 }
 
@@ -24,10 +24,13 @@ resource "azurerm_subnet" "dns_resolver" {
   name                 = "DnsResolverSubnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [cidrsubnet(var.address_space, 5, 1)]
+  address_prefixes     = [cidrsubnet(var.address_space, 2, 1)]
 
 }
 
+# minimum subnet size of /26
+# recommendation of /24 for future support of WAF
+# https://learn.microsoft.com/en-us/azure/application-gateway/configuration-infrastructure
 resource "azurerm_subnet" "app_gateway" {
 
   name                 = "AppGatewaySubnet"
@@ -36,22 +39,22 @@ resource "azurerm_subnet" "app_gateway" {
   address_prefixes     = [cidrsubnet(var.address_space, 2, 2)]
 
 }
-
+/*
 resource "azurerm_subnet" "shared" {
 
   name                 = "snet-shared"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [cidrsubnet(var.address_space, 2, 3)]
+  address_prefixes     = [cidrsubnet(var.address_space, 2, 2)]
 
-}
+}*/
 
 resource "azurerm_subnet" "workload" {
 
   name                 = "snet-workload"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [cidrsubnet(var.address_space, 2, 4)]
+  address_prefixes     = [cidrsubnet(var.address_space, 2, 3)]
 
 }
 
