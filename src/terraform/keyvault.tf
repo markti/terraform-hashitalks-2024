c52c981ba358a0dc22d601a1787968a634dc86a2
markti <markti@outlook.com>
2024-01-31 08:25:32 -0500

    keyvault terraform access

---
+++ src/terraform/keyvault.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "main" {
+  name                          = "kv-${var.application_name}-${var.environment_name}-${random_string.main.result}"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  enable_rbac_authorization     = true
+  public_network_access_enabled = false
+  purge_protection_enabled      = false
+}
+
+resource "azurerm_role_assignment" "terraform_keyvault_access" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
