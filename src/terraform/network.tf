ca66f442c80488af9e28a460c4abf3ae1653dfe0
markti <markti@outlook.com>
2024-01-30 21:48:41 -0500

    id

--- src/terraform/network.tf
+++ src/terraform/network.tf
@@ -47,7 +47,7 @@ module "azure-terraformer" {
   source  = "markti/azure-terraformer/azurerm//modules/monitor/diagnostic-setting/rando"
   version = "1.0.7"
 
-  resource_id                = azurerm_virtual_network.main.resource_id
+  resource_id                = azurerm_virtual_network.main.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
   logs                       = ["VMProtectionAlerts"]
 
