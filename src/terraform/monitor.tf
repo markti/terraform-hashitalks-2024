9e26da1e58408464a427c2bceda36a2549a095b5
markti <markti@outlook.com>
2024-02-01 14:18:50 -0500

    appi

--- src/terraform/monitor.tf
+++ src/terraform/monitor.tf
@@ -5,3 +5,11 @@ resource "azurerm_log_analytics_workspace" "main" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+resource "azurerm_application_insights" "main" {
+  name                = "appi-${var.application_name}-${var.environment_name}-${random_string.main.result}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  workspace_id        = azurerm_log_analytics_workspace.main.id
+  application_type    = "web"
+}
