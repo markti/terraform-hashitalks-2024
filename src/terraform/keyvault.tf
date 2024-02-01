0b6bee2612c6a0a568912b2dbf10bb6bc7a34311
markti <markti@outlook.com>
2024-01-31 23:56:59 -0500

    kv gh runner

--- src/terraform/keyvault.tf
+++ src/terraform/keyvault.tf
@@ -46,6 +46,13 @@ resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_workload" {
   virtual_network_id    = azurerm_virtual_network.main.id
 }
 
+resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_github_runner" {
+  name                  = "dns-link-keyvault-github-runner"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.keyvault.name
+  virtual_network_id    = data.azurerm_virtual_network.github_runner.id
+}
+
 resource "azurerm_private_endpoint" "keyvault" {
   name                = "pep-${azurerm_key_vault.main.name}"
   resource_group_name = azurerm_resource_group.main.name
