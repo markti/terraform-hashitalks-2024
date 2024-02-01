b499c778c7d8f3f1539f99fcd7c0ab44e17f2d58
markti <markti@outlook.com>
2024-02-01 10:44:35 -0500

    health check

---
+++ src/dotnet/healthcheck/push-image.sh
@@ -0,0 +1,10 @@
+ACR=craztflabdevagyhy3jv.azurecr.io
+IMAGE=health-check
+TAG=v2024.01.1
+
+set -e
+az acr login --name $ACR
+
+docker buildx build --platform linux/amd64 -t $ACR/$IMAGE:$TAG -f Dockerfile .
+
+docker push $ACR/$IMAGE:$TAG
