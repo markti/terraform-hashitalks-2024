7d93c94a144e2f551582e1f9bd6f2129f90e1b0e
markti <markti@outlook.com>
2024-02-01 19:40:53 -0500

    azurerm

--- src/terraform/k8s/versions.tf
+++ src/terraform/k8s/versions.tf
@@ -13,6 +13,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
 data "azurerm_kubernetes_cluster" "main" {
   name                = "aks-aztflab-dev-agyhy3jv"
   resource_group_name = "rg-aztflab-dev-agyhy3jv"
