a9552fa825691bf2bdb5c94e61a06602934c420e
markti <markti@outlook.com>
2024-02-01 19:35:23 -0500

    try opt

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -2,5 +2,5 @@ output "registry_endpoint" {
   value = azurerm_container_registry.main.login_server
 }
 output "aks_host" {
-  value = try(azurerm_kubernetes_cluster.main.kube_admin_config[0].host)
+  value = try(azurerm_kubernetes_cluster.main.kube_admin_config[0].host, "")
 }
