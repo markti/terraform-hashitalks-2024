8c0c92596f0c9d232602b819ea8823138416cf91
markti <markti@outlook.com>
2024-02-01 19:43:42 -0500

    sensitive host

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -2,5 +2,6 @@ output "registry_endpoint" {
   value = azurerm_container_registry.main.login_server
 }
 output "aks_host" {
-  value = try(azurerm_kubernetes_cluster.main.kube_admin_config[0].host, "")
+  value     = try(azurerm_kubernetes_cluster.main.kube_admin_config[0].host, "")
+  sensitive = true
 }
