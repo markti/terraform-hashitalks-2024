eb2db44269eb39e54558a1e1df778d9e78f1de9f
markti <markti@outlook.com>
2024-02-03 17:16:19 -0500

    add role assignment

--- src/terraform/infra/cosmos.tf
+++ src/terraform/infra/cosmos.tf
@@ -29,3 +29,44 @@ resource "azurerm_role_assignment" "workload_identity_cosmos_reader" {
   role_definition_name = "DocumentDB Account Contributor"
   principal_id         = azurerm_user_assigned_identity.workload.principal_id
 }
+
+resource "azurerm_cosmosdb_sql_role_definition" "reader" {
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  name                = "CosmosDB Reader"
+  assignable_scopes   = ["/"]
+
+  permissions {
+    data_actions = [
+      "Microsoft.DocumentDB/databaseAccounts/readMetadata",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/read",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/executeQuery",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/readChangeFeed"
+    ]
+  }
+}
+
+resource "azurerm_cosmosdb_sql_role_definition" "writer" {
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  name                = "CosmosDB Writer"
+  assignable_scopes   = ["/"]
+
+  permissions {
+    data_actions = [
+      "Microsoft.DocumentDB/databaseAccounts/readMetadata",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/*",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/*"
+    ]
+  }
+}
+
+resource "azurerm_cosmosdb_sql_role_assignment" "workload_identity" {
+  name                = azurerm_user_assigned_identity.workload.principal_id
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+
+  scope              = azurerm_cosmosdb_account.main.id
+  role_definition_id = azurerm_cosmosdb_sql_role_definition.writer.id
+  principal_id       = azurerm_user_assigned_identity.workload.principal_id
+}
