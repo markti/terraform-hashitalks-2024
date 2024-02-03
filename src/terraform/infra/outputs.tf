88693ff8745d416a9ed27614dddd4d61bd3e0128
markti <markti@outlook.com>
2024-02-03 03:39:24 -0500

    client id

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -8,7 +8,7 @@ output "container_registry" {
   value = azurerm_container_registry.main.login_server
 }
 output "workload_managed_identity_id" {
-  value = azurerm_user_assigned_identity.workload.principal_id
+  value = azurerm_user_assigned_identity.workload.client_id
 }
 output "keyvault_name" {
   value = azurerm_key_vault.main.name
