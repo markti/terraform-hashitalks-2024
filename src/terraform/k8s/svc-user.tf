b210312cf764ab4613bc9a56c934a99562bd6cb9
markti <markti@outlook.com>
2024-02-03 18:33:59 -0500

    underscore

--- src/terraform/k8s/svc-user.tf
+++ src/terraform/k8s/svc-user.tf
@@ -161,8 +161,8 @@ resource "kubernetes_config_map" "user_svc" {
 
   data = {
     HELLO_WORLD         = "Azure Terraformer"
-    "CosmosDb:Endpoint" = var.cosmos_endpoint
-    "CosmosDb:Database" = var.user_svc_database
+    "CosmosDb_Endpoint" = var.cosmos_endpoint
+    "CosmosDb_Database" = var.user_svc_database
   }
 
 }
