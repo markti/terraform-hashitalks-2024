3b5a5dbc237f805eecb3f16e128a7a0234c0bfd8
markti <markti@outlook.com>
2024-02-03 22:22:09 -0500

    add http client

--- src/dotnet/healthcheck/Startup.cs
+++ src/dotnet/healthcheck/Startup.cs
@@ -27,6 +27,8 @@ namespace HealthCheck
 
             services.AddApplicationInsightsTelemetry();
 
+            services.AddHttpClient();
+
             services.AddHealthChecks()
                 .AddCheck<DoNothingHealthCheck>("nada");
         }
