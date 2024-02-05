fdfadfdbbb8bc308bbae746a12aa1bcee89a1f2e
markti <markti@outlook.com>
2024-02-05 17:26:17 -0500

    set

--- src/terraform/infra/policy.tf
+++ src/terraform/infra/policy.tf
@@ -5,7 +5,7 @@ data "azurerm_policy_set_definition" "az_resiliency" {
 resource "azurerm_resource_group_policy_assignment" "az_resiliency" {
   name                 = "pol-az-resiliency"
   resource_group_id    = azurerm_resource_group.main.id
-  policy_definition_id = data.azurerm_policy_definition.az_resiliency.id
+  policy_definition_id = data.azurerm_policy_set_definition.az_resiliency.id
 
   parameters = <<PARAMS
     {
