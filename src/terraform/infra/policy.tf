8ed29d338338ba1bd78a86b50caab9fc94394730
markti <markti@outlook.com>
2024-02-06 20:55:10 -0500

    azure policy

--- src/terraform/infra/policy.tf
+++ src/terraform/infra/policy.tf
@@ -6,18 +6,6 @@ resource "azurerm_resource_group_policy_assignment" "az_resiliency" {
   name                 = "pol-az-resiliency"
   resource_group_id    = azurerm_resource_group.main.id
   policy_definition_id = data.azurerm_policy_set_definition.az_resiliency.id
-  /*
-  parameters = <<PARAMS
-    {
-      "effect": {
-        "value": "Audit"
-      },
-      "allow": {
-        "value": "Both"
-      }
-    }
-PARAMS
-*/
 
   parameters = jsonencode({
     effect = {
