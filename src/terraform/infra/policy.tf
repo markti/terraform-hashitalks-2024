c9960252f69867ccc7c6eff59d7c50d43dfe1ab3
markti <markti@outlook.com>
2024-02-07 19:09:21 -0500

    no params

--- src/terraform/infra/policy.tf
+++ src/terraform/infra/policy.tf
@@ -17,3 +17,19 @@ resource "azurerm_resource_group_policy_assignment" "az_resiliency" {
   })
 
 }
+
+
+data "azurerm_policy_set_definition" "cis_v2" {
+  name = "06f19060-9e68-4070-92ca-f15cc126059e"
+}
+
+
+resource "azurerm_resource_group_policy_assignment" "cis_v2" {
+  name                 = "pol-cis-v2"
+  resource_group_id    = azurerm_resource_group.main.id
+  policy_definition_id = data.azurerm_policy_set_definition.cis_v2.id
+
+  parameters = jsonencode({
+  })
+
+}
